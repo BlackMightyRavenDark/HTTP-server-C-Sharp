49463577dede28402a4f22e08295e24be8fcf61d
BlackRain <masaraksh86@mail.ru>
2023-04-09 14:53:02 +0700

    Создан GuiServer

---
+++ GuiServer/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace GuiServer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStartServer = new System.Windows.Forms.Button();
+            this.btnStopServer = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDownServerPort = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownServerPort)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnStartServer
+            // 
+            this.btnStartServer.Location = new System.Drawing.Point(169, 11);
+            this.btnStartServer.Name = "btnStartServer";
+            this.btnStartServer.Size = new System.Drawing.Size(75, 23);
+            this.btnStartServer.TabIndex = 0;
+            this.btnStartServer.Text = "Start";
+            this.btnStartServer.UseVisualStyleBackColor = true;
+            this.btnStartServer.Click += new System.EventHandler(this.btnStartServer_Click);
+            // 
+            // btnStopServer
+            // 
+            this.btnStopServer.Enabled = false;
+            this.btnStopServer.Location = new System.Drawing.Point(250, 11);
+            this.btnStopServer.Name = "btnStopServer";
+            this.btnStopServer.Size = new System.Drawing.Size(75, 23);
+            this.btnStopServer.TabIndex = 1;
+            this.btnStopServer.Text = "Stop";
+            this.btnStopServer.UseVisualStyleBackColor = true;
+            this.btnStopServer.Click += new System.EventHandler(this.btnStopServer_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Порт сервера:";
+            // 
+            // numericUpDownServerPort
+            // 
+            this.numericUpDownServerPort.Location = new System.Drawing.Point(102, 12);
+            this.numericUpDownServerPort.Maximum = new decimal(new int[] {
+            65000,
+            0,
+            0,
+            0});
+            this.numericUpDownServerPort.Minimum = new decimal(new int[] {
+            1025,
+            0,
+            0,
+            0});
+            this.numericUpDownServerPort.Name = "numericUpDownServerPort";
+            this.numericUpDownServerPort.Size = new System.Drawing.Size(61, 20);
+            this.numericUpDownServerPort.TabIndex = 3;
+            this.numericUpDownServerPort.Value = new decimal(new int[] {
+            5555,
+            0,
+            0,
+            0});
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(347, 56);
+            this.Controls.Add(this.numericUpDownServerPort);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnStopServer);
+            this.Controls.Add(this.btnStartServer);
+            this.Name = "Form1";
+            this.Text = "HTTP server";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownServerPort)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStartServer;
+        private System.Windows.Forms.Button btnStopServer;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDownServerPort;
+    }
+}
+
---
+++ GuiServer/Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GuiServer
+{
+    public partial class Form1 : Form
+    {
+        private Socket server = null;
+        private bool active = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStartServer_Click(object sender, EventArgs e)
+        {
+            StartServer();
+        }
+
+        private void btnStopServer_Click(object sender, EventArgs e)
+        {
+            StopServer(server);
+        }
+
+        private void StartServer()
+        {
+            btnStartServer.Enabled = false;
+            numericUpDownServerPort.Enabled = false;
+            btnStopServer.Enabled = true;
+
+            try
+            {
+                IPEndPoint endPoint = new IPEndPoint(IPAddress.Any, (int)numericUpDownServerPort.Value);
+                server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                server.Bind(endPoint);
+                server.Listen((int)SocketOptionName.MaxConnections);
+
+                active = true;
+                Task.Run(() =>
+                {
+                    while (active)
+                    {
+                        try
+                        {
+                            Socket client = server.Accept();
+                            LogEvent($"{client.RemoteEndPoint} is connected");
+
+                            ProcessClient(client);
+                            DisconnectClient(client);
+                        }
+                        catch (Exception ex)
+                        {
+                            System.Diagnostics.Debug.WriteLine(ex.Message);
+                            active = false;
+                        }
+                    }
+                });
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                server = null;
+                active = false;
+                btnStartServer.Enabled = true;
+                btnStopServer.Enabled = false;
+                numericUpDownServerPort.Enabled = true;
+            }
+        }
+
+        private void ProcessClient(Socket client)
+        {
+            byte[] buffer = new byte[ushort.MaxValue];
+            int bytesRead = client.Receive(buffer, 0, buffer.Length, SocketFlags.None);
+            if (bytesRead == 0)
+            {
+                LogEvent($"Zero bytes received from {client.RemoteEndPoint}");
+                return;
+            }
+
+            string msg = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+            LogEvent($"{client.RemoteEndPoint} sent:\n<<<\n{msg}\n>>>");
+
+            string[] strings = msg.Split(new string[] { "\r\n" }, StringSplitOptions.None);
+            string[] request = strings[0].Split(new char[] { ' ' }, 3);
+            string answer = request[0] == "GET" ?
+                GenerateResponse(200, "OK", "OK") :
+                GenerateResponse(400, "Bad request", null);
+            SendMessage(client, answer);
+        }
+
+        private void SendMessage(Socket client, string msg)
+        {
+            byte[] msgBytes = Encoding.UTF8.GetBytes(msg);
+            client.Send(msgBytes);
+        }
+
+        private void DisconnectClient(Socket client)
+        {
+            LogEvent($"{client.RemoteEndPoint} is disconnected");
+            client.Shutdown(SocketShutdown.Both);
+            client.Close();
+        }
+
+        private static string GenerateResponse(int errorCode, string msg, string body)
+        {
+            string t = $"HTTP/1.1 {errorCode} {msg}\r\n" +
+                "Access-Control-Allow-Origin: *\r\n";
+            if (!string.IsNullOrEmpty(body))
+            {
+                byte[] bodyBytes = Encoding.UTF8.GetBytes(body);
+                t += "Content-Type: text/plain; charset=UTF-8\r\n" +
+                    $"Content-Length: {bodyBytes.Length}\r\n\r\n{body}";
+            }
+            else
+            {
+                t += "Content-Length: 0\r\n\r\n";
+            }
+            return t;
+        }
+
+        private void LogEvent(string eventText)
+        {
+            string dateTime = DateTime.Now.ToString("yyyy.MM.dd hh:mm:ss");
+            System.Diagnostics.Debug.WriteLine($"{dateTime}> {eventText}");
+        }
+
+        private void StopServer(Socket serverSocket)
+        {
+            if (serverSocket != null)
+            {
+                try
+                {
+                    serverSocket.Shutdown(SocketShutdown.Both);
+                    serverSocket.Close();
+                    serverSocket = null;
+                }
+                catch (Exception ex)
+                {
+                    System.Diagnostics.Debug.WriteLine(ex.Message);
+                    if (ex is SocketException)
+                    {
+                        System.Diagnostics.Debug.WriteLine($"Socket error {(ex as SocketException).ErrorCode}");
+                    }
+                    serverSocket.Close();
+                    serverSocket = null;
+                }
+            }
+
+            btnStopServer.Enabled = false;
+            numericUpDownServerPort.Enabled = true;
+            btnStartServer.Enabled = true;
+        }
+    }
+}
