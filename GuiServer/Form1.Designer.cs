1da5984970b818f54f2a09df2f767ffc96a35ab6
BlackRain <masaraksh86@mail.ru>
2023-04-14 13:11:44 +0700

    GuiServer: Добавлена многопоточная обработка клиентов

--- GuiServer/Form1.Designer.cs
+++ GuiServer/Form1.Designer.cs
@@ -30,8 +30,11 @@
         {
             this.btnStartServer = new System.Windows.Forms.Button();
             this.btnStopServer = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
+            this.labelPort = new System.Windows.Forms.Label();
             this.numericUpDownServerPort = new System.Windows.Forms.NumericUpDown();
+            this.labelLog = new System.Windows.Forms.Label();
+            this.listBoxLog = new System.Windows.Forms.ListBox();
+            this.checkBoxAutoscroll = new System.Windows.Forms.CheckBox();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDownServerPort)).BeginInit();
             this.SuspendLayout();
             // 
@@ -56,14 +59,14 @@
             this.btnStopServer.UseVisualStyleBackColor = true;
             this.btnStopServer.Click += new System.EventHandler(this.btnStopServer_Click);
             // 
-            // label1
+            // labelPort
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(16, 16);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(80, 13);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "Порт сервера:";
+            this.labelPort.AutoSize = true;
+            this.labelPort.Location = new System.Drawing.Point(16, 16);
+            this.labelPort.Name = "labelPort";
+            this.labelPort.Size = new System.Drawing.Size(80, 13);
+            this.labelPort.TabIndex = 2;
+            this.labelPort.Text = "Порт сервера:";
             // 
             // numericUpDownServerPort
             // 
@@ -87,17 +90,58 @@
             0,
             0});
             // 
+            // labelLog
+            // 
+            this.labelLog.AutoSize = true;
+            this.labelLog.Location = new System.Drawing.Point(12, 47);
+            this.labelLog.Name = "labelLog";
+            this.labelLog.Size = new System.Drawing.Size(73, 13);
+            this.labelLog.TabIndex = 4;
+            this.labelLog.Text = "Лог собитий:";
+            // 
+            // listBoxLog
+            // 
+            this.listBoxLog.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBoxLog.BackColor = System.Drawing.Color.Black;
+            this.listBoxLog.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.listBoxLog.ForeColor = System.Drawing.Color.Lime;
+            this.listBoxLog.FormattingEnabled = true;
+            this.listBoxLog.ItemHeight = 20;
+            this.listBoxLog.Location = new System.Drawing.Point(12, 74);
+            this.listBoxLog.Name = "listBoxLog";
+            this.listBoxLog.Size = new System.Drawing.Size(574, 164);
+            this.listBoxLog.TabIndex = 5;
+            // 
+            // checkBoxAutoscroll
+            // 
+            this.checkBoxAutoscroll.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.checkBoxAutoscroll.AutoSize = true;
+            this.checkBoxAutoscroll.Checked = true;
+            this.checkBoxAutoscroll.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBoxAutoscroll.Location = new System.Drawing.Point(484, 244);
+            this.checkBoxAutoscroll.Name = "checkBoxAutoscroll";
+            this.checkBoxAutoscroll.Size = new System.Drawing.Size(102, 17);
+            this.checkBoxAutoscroll.TabIndex = 6;
+            this.checkBoxAutoscroll.Text = "Автопрокрутка";
+            this.checkBoxAutoscroll.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(347, 56);
+            this.ClientSize = new System.Drawing.Size(598, 270);
+            this.Controls.Add(this.checkBoxAutoscroll);
+            this.Controls.Add(this.listBoxLog);
+            this.Controls.Add(this.labelLog);
             this.Controls.Add(this.numericUpDownServerPort);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.labelPort);
             this.Controls.Add(this.btnStopServer);
             this.Controls.Add(this.btnStartServer);
             this.Name = "Form1";
             this.Text = "HTTP server";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDownServerPort)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -108,8 +152,11 @@
 
         private System.Windows.Forms.Button btnStartServer;
         private System.Windows.Forms.Button btnStopServer;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelPort;
         private System.Windows.Forms.NumericUpDown numericUpDownServerPort;
+        private System.Windows.Forms.Label labelLog;
+        private System.Windows.Forms.ListBox listBoxLog;
+        private System.Windows.Forms.CheckBox checkBoxAutoscroll;
     }
 }
 
--- GuiServer/Form1.cs
+++ GuiServer/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
@@ -11,10 +12,16 @@ namespace GuiServer
     {
         private Socket server = null;
         private bool active = false;
+        private List<Socket> clientList;
 
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            StopServer(server);
         }
 
         private void btnStartServer_Click(object sender, EventArgs e)
@@ -33,12 +40,17 @@ namespace GuiServer
             numericUpDownServerPort.Enabled = false;
             btnStopServer.Enabled = true;
 
+            int serverPort = (int)numericUpDownServerPort.Value;
             try
             {
-                IPEndPoint endPoint = new IPEndPoint(IPAddress.Any, (int)numericUpDownServerPort.Value);
+                IPEndPoint endPoint = new IPEndPoint(IPAddress.Any, serverPort);
                 server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
                 server.Bind(endPoint);
                 server.Listen((int)SocketOptionName.MaxConnections);
+
+                clientList = new List<Socket>();
+
+                LogEvent($"Server started on port {serverPort}");
 
                 active = true;
                 Task.Run(() =>
@@ -50,8 +62,11 @@ namespace GuiServer
                             Socket client = server.Accept();
                             LogEvent($"{client.RemoteEndPoint} is connected");
 
-                            ProcessClient(client);
-                            DisconnectClient(client);
+                            Task.Run(() =>
+                            {
+                                ProcessClient(client);
+                                DisconnectClient(client);
+                            });
                         }
                         catch (Exception ex)
                         {
@@ -63,7 +78,8 @@ namespace GuiServer
             }
             catch (Exception ex)
             {
-                MessageBox.Show(ex.Message);
+                MessageBox.Show($"Ошибка запуска сервера!\n{ex.Message}", "Ошибка!",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
                 server = null;
                 active = false;
                 btnStartServer.Enabled = true;
@@ -74,6 +90,8 @@ namespace GuiServer
 
         private void ProcessClient(Socket client)
         {
+            AddClient(client);
+
             byte[] buffer = new byte[ushort.MaxValue];
             int bytesRead = client.Receive(buffer, 0, buffer.Length, SocketFlags.None);
             if (bytesRead == 0)
@@ -83,9 +101,9 @@ namespace GuiServer
             }
 
             string msg = Encoding.UTF8.GetString(buffer, 0, bytesRead);
-            LogEvent($"{client.RemoteEndPoint} sent:\n<<<\n{msg}\n>>>");
 
             string[] strings = msg.Split(new string[] { "\r\n" }, StringSplitOptions.None);
+            LogEvent($"{client.RemoteEndPoint} sent: {strings[0]}");
             string[] request = strings[0].Split(new char[] { ' ' }, 3);
             string answer = request[0] == "GET" ?
                 GenerateResponse(200, "OK", "OK") :
@@ -99,11 +117,51 @@ namespace GuiServer
             client.Send(msgBytes);
         }
 
-        private void DisconnectClient(Socket client)
+        private void DisconnectClient(Socket client, bool autoRemove = true)
         {
             LogEvent($"{client.RemoteEndPoint} is disconnected");
+            if (autoRemove)
+            {
+                RemoveClient(client);
+            }
             client.Shutdown(SocketShutdown.Both);
             client.Close();
+        }
+
+        private void DisconnectAllClients()
+        {
+            if (clientList != null)
+            {
+                clientList.ForEach((client) =>
+                {
+                    DisconnectClient(client, false);
+                });
+                clientList.Clear();
+            }
+        }
+
+        private void AddClient(Socket client)
+        {
+            if (InvokeRequired)
+            {
+                Invoke((MethodInvoker)delegate { AddClient(client); });
+            }
+            else
+            {
+                clientList.Add(client);
+            }
+        }
+
+        private void RemoveClient(Socket client)
+        {
+            if (InvokeRequired)
+            {
+                Invoke((MethodInvoker)delegate { RemoveClient(client); });
+            }
+            else
+            {
+                clientList.Remove(client);
+            }
         }
 
         private static string GenerateResponse(int errorCode, string msg, string body)
@@ -125,8 +183,19 @@ namespace GuiServer
 
         private void LogEvent(string eventText)
         {
-            string dateTime = DateTime.Now.ToString("yyyy.MM.dd hh:mm:ss");
-            System.Diagnostics.Debug.WriteLine($"{dateTime}> {eventText}");
+            if (InvokeRequired)
+            {
+                Invoke((MethodInvoker)delegate { LogEvent(eventText); });
+            }
+            else
+            {
+                string dateTime = DateTime.Now.ToString("yyyy.MM.dd hh:mm:ss");
+                listBoxLog.Items.Add($"{dateTime}> {eventText}");
+                if (checkBoxAutoscroll.Checked)
+                {
+                    listBoxLog.SelectedIndex = listBoxLog.Items.Count - 1;
+                }
+            }
         }
 
         private void StopServer(Socket serverSocket)
@@ -135,6 +204,7 @@ namespace GuiServer
             {
                 try
                 {
+                    DisconnectAllClients();
                     serverSocket.Shutdown(SocketShutdown.Both);
                     serverSocket.Close();
                     serverSocket = null;
@@ -149,6 +219,8 @@ namespace GuiServer
                     serverSocket.Close();
                     serverSocket = null;
                 }
+
+                LogEvent("Server stopped!");
             }
 
             btnStopServer.Enabled = false;
